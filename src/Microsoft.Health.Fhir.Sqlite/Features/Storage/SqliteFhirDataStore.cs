5e8b7a00b146aaba8f8af02c79622e2dba453359
Robin Todd <rotodd@microsoft.com>
2020-07-29 18:36:51 -0700

    Uses info from client to create resource.

--- src/Microsoft.Health.Fhir.Sqlite/Features/Storage/SqliteFhirDataStore.cs
+++ src/Microsoft.Health.Fhir.Sqlite/Features/Storage/SqliteFhirDataStore.cs
@@ -8,11 +8,9 @@ using System.Data.SQLite;
 using System.Threading;
 using System.Threading.Tasks;
 using EnsureThat;
-using Microsoft.Extensions.Logging;
 using Microsoft.Health.Extensions.DependencyInjection;
 using Microsoft.Health.Fhir.Core.Features.Conformance;
 using Microsoft.Health.Fhir.Core.Features.Persistence;
-using Microsoft.Health.Fhir.Core.Models;
 using Microsoft.Health.Fhir.Sqlite.Configs;
 using Task = System.Threading.Tasks.Task;
 
@@ -23,43 +21,45 @@ namespace Microsoft.Health.Fhir.Sqlite.Features.Storage
     /// </summary>
     public class SqliteFhirDataStore : IFhirDataStore, IProvideCapability, IStartable
     {
-        private readonly IModelInfoProvider _modelInfoProvider;
-        private readonly ILogger<SqliteFhirDataStore> _logger;
         private readonly string _connectionString;
         private readonly string _databaseFileName;
 
         public SqliteFhirDataStore(
-            SqliteDataStoreConfiguration configuration,
-            IModelInfoProvider modelInfoProvider,
-            ILogger<SqliteFhirDataStore> logger)
+            SqliteDataStoreConfiguration configuration)
         {
             EnsureArg.IsNotNull(configuration, nameof(configuration));
-            EnsureArg.IsNotNull(modelInfoProvider, nameof(modelInfoProvider));
-            EnsureArg.IsNotNull(logger, nameof(logger));
-
-            _modelInfoProvider = modelInfoProvider;
-            _logger = logger;
 
             _connectionString = configuration.ConnectionString;
             _databaseFileName = configuration.DatabaseFileName;
         }
 
-        // TODO: Add check if resource exists.
+        // TODO: Add check if resource exists - right now, this only creates new resources.
         public Task<UpsertOutcome> UpsertAsync(ResourceWrapper resource, WeakETag weakETag, bool allowCreate, bool keepHistory, CancellationToken cancellationToken)
         {
             var connection = new SQLiteConnection(_connectionString);
-            connection.Open();
+            string sql = @"
+                INSERT INTO Resource
+                    (ResourceTypeName, ResourceId, Version, RawResource)
+                VALUES
+                    (@resourceTypeName, @resourceId, 1, @rawResource)";
 
-            // TODO: parameterize, add resource values from client
-            string sql = @"INSERT INTO Resource (ResourceTypeId, ResourceId, RawResource) VALUES (1, 'Patient', 'Test')";
+            try
+            {
+                connection.Open();
+                using (var command = new SQLiteCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@resourceTypeName", resource.ResourceTypeName);
+                    command.Parameters.AddWithValue("@resourceId", resource.ResourceId);
+                    command.Parameters.AddWithValue("@rawResource", resource.RawResource.Data);
 
-            var command = new SQLiteCommand(sql, connection);
-            var newVersion = command.ExecuteNonQuery();
-
-            connection.Close();
-
-            // TODO: Add version info to resource table
-            resource.Version = newVersion.ToString();
+                    command.ExecuteNonQuery();
+                    resource.Version = "1";
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
 
             return Task.FromResult(new UpsertOutcome(resource, SaveOutcomeType.Created));
         }
@@ -92,7 +92,14 @@ namespace Microsoft.Health.Fhir.Sqlite.Features.Storage
             connection.Open();
 
             // TODO: Update RawResource to blob?
-            string sql = "CREATE TABLE IF NOT EXISTS Resource (ResourceTypeId int, ResourceId varchar(64), RawResource varchar(256))";
+            // TODO: Replace resource type name with ID
+            string sql = @"CREATE TABLE IF NOT EXISTS Resource (
+                ResourceTypeName varchar(64) NOT NULL,
+                ResourceId varchar(64) NOT NULL,
+                Version int NOT NULL,
+                RawResource varchar(65536))";
+
+            // RawResource varchar(256))";
 
             var command = new SQLiteCommand(sql, connection);
             command.ExecuteNonQuery();
